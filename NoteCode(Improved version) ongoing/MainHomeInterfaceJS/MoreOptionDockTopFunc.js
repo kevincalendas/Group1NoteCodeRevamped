54f6e7d5d2ffbb2cadad35f0df658da48af5c6c2
kevincalendas <johnkevixcalendas@gmail.com>
2025-12-12 19:32:37 +0800

    test

--- NoteCode(Improved version) ongoing/MainHomeInterfaceJS/MoreOptionDockTopFunc.js
+++ NoteCode(Improved version) ongoing/MainHomeInterfaceJS/MoreOptionDockTopFunc.js
@@ -1,5 +1,6 @@
 function MainOptionsOpen() {
     const MainOptionsPanel = document.getElementById("MoreOptionWindow");
+    const MainButtonToggleOptions = document.getElementById("MoreOptionMenuButton");
 
     if (MainOptionsPanel.style.display === "none" || MainOptionsPanel.style.display === "") {
         MainOptionsPanel.style.animationName = "OpenFadeEffect";
@@ -8,13 +9,27 @@ function MainOptionsOpen() {
         MainOptionsPanel.style.display = "grid";    
         MainOptionsPanel.style.opacity = "1";
         MainOptionsPanel.style.transform = "translate(0px, 0px)";
+
+        MainButtonToggleOptions.style.animationName = "ButtonClicked";
+        MainButtonToggleOptions.style.animationFillMode = "forwards";
+        MainButtonToggleOptions.style.animationDuration = "0.8s";
+
+        setTimeout(() => {
+            MainButtonToggleOptions.classList.remove('ButtonClicked');
+            MainButtonToggleOptions.classList.add('ButtonClicked');
+        }, 100);
     } else {
         MainOptionsPanel.style.animationName = "CloseFadeEffect";
         MainOptionsPanel.style.animationDuration = "0.6s";
         MainOptionsPanel.style.animationFillMode = "forwards";
         
+        MainButtonToggleOptions.style.animationName = "ButtonClicked";
+        MainButtonToggleOptions.style.animationFillMode = "forwards";
+        MainButtonToggleOptions.style.animationDuration = "0.8s";
         setTimeout(() => {
             MainOptionsPanel.style.display = "none";
+            MainButtonToggleOptions.classList.remove('ButtonClicked');
+            MainButtonToggleOptions.classList.add('ButtonClicked');
         }, 500);
     }
 }
