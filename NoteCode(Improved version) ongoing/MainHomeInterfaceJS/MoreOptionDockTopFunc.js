b702d54c68a21b1c19c0adc2463bf7876053b481
kevincalendas <johnkevixcalendas@gmail.com>
2025-12-12 18:33:36 +0800

    test

--- NoteCode(Improved version) ongoing/MainHomeInterfaceJS/MoreOptionDockTopFunc.js
+++ NoteCode(Improved version) ongoing/MainHomeInterfaceJS/MoreOptionDockTopFunc.js
@@ -2,15 +2,15 @@ function MainOptionsOpen() {
     const MainOptionsPanel = document.getElementById("MoreOptionWindow");
 
     if (MainOptionsPanel.style.display === "none" || MainOptionsPanel.style.display === "") {
-        MainOptionsPanel.style.animationName = "OpenTransformEffect, OpenScaleEffect";
-        MainOptionsPanel.style.animationDuration = "0.4s, 0.5s";
+        MainOptionsPanel.style.animationName = "OpenFadeEffect";
+        MainOptionsPanel.style.animationDuration = "0.4s";
         MainOptionsPanel.style.animationFillMode = "forwards";
         MainOptionsPanel.style.display = "grid";    
         MainOptionsPanel.style.opacity = "1";
         MainOptionsPanel.style.transform = "translate(0px, 0px)";
     } else {
-        MainOptionsPanel.style.animationName = "CloseTransformEffect, CloseScaleEffect";
-        MainOptionsPanel.style.animationDuration = "0.4s, 0.5s";
+        MainOptionsPanel.style.animationName = "CloseFadeEffect";
+        MainOptionsPanel.style.animationDuration = "0.6s";
         MainOptionsPanel.style.animationFillMode = "forwards";
         
         setTimeout(() => {
